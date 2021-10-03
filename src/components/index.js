84849685e9164d7546221bf396ba2d5f7198e5b9
Denilson Mucavele <denilsonmucavel@outlook.com>
2021-10-02 18:44:27 -0700

    updating the index .js

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Chart } from '../components/Chart/Chart';
+export { default as CountryPicker } from '../components/CountryPicker/CountryPicker';
+export { default as Cards } from '../components/Cards/Cards';
+export { default as Cards } from '../components/Cards';
